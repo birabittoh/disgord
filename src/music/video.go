9377cb25dc3ee81ecdad1b30e225809482537a1a
Marco Andronaco <andronacomarco@gmail.com>
2024-10-27 21:28:57 +0100

    minor bugfix

--- src/music/video.go
+++ src/music/video.go
@@ -1,8 +1,6 @@
 package music
 
 import (
-	"strings"
-
 	gl "github.com/birabittoh/disgord/src/globals"
 	"github.com/kkdai/youtube/v2"
 )
@@ -11,10 +9,6 @@ func getVideo(args []string) (*youtube.Video, error) {
 	video, err := yt.GetVideo(args[0])
 	if err == nil {
 		return video, nil
-	}
-
-	if !strings.HasPrefix(err.Error(), "extractVideoID failed") {
-		return nil, err
 	}
 
 	id, err := gl.Search(args)
